9f025ff4188d5e02cf1b5f3e6a4232d934cf64c1
Jakub Zięba <jakubzieba7@gmail.com>
2023-09-23 13:04:02 +0200

    Added Task view and Task view model

---
+++ Core/ViewModels/TaskViewModel.cs
@@ -0,0 +1,12 @@
+﻿using MyTasks.Core.Models.Domains;
+using Task = MyTasks.Core.Models.Domains.Task;
+
+namespace MyTasks.Core.ViewModels
+{
+    public class TaskViewModel
+    {
+        public string Heading { get; set; }
+        public Task Task { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+    }
+}
