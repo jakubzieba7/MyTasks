fe1edbd001f1f56e68319c8deb072b9182360321
Jakub Zięba <jakubzieba7@gmail.com>
2023-09-30 10:46:55 +0200

    Added new migration and modified Category domain - added dependency to the UserId

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyTasks.Core.Models;
+using MyTasks.Core.Service;
+using MyTasks.Core.ViewModels;
+using MyTasks.Persistence.Extensions;
+
+namespace MyTasks.Controllers
+{
+    public class CategoryController : Controller
+    {
+        
+    }
+}
--- Core/Models/Domains/Category.cs
+++ Core/Models/Domains/Category.cs
@@ -12,6 +12,8 @@ namespace MyTasks.Core.Models.Domains
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        public string UserId { get; set; }
+        public ApplicationUser User { get; set; }
         public ICollection<Task> Tasks { get; set; }
     }
 }
