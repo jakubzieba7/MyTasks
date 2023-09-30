6934f1bcafd22fa823052b2b2e2c527d439da7bd
Jakub Zięba <jakubzieba7@gmail.com>
2023-09-30 12:12:48 +0200

    Created Category and Categories actions in controller and Get and GetCategories methods in repository

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,13 +1,52 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using MyTasks.Core.Models;
+using MyTasks.Core.Models.Domains;
 using MyTasks.Core.Service;
 using MyTasks.Core.ViewModels;
+using MyTasks.Persistence;
 using MyTasks.Persistence.Extensions;
+using MyTasks.Persistence.Repositories;
 
 namespace MyTasks.Controllers
 {
+    [Authorize]
     public class CategoryController : Controller
     {
+        private CategoryRepository _categoryRepository;
+
+        public CategoryController(ApplicationDbContext _context)
+        {
+            _categoryRepository = new CategoryRepository(_context);
+        }
+
+
+        public IActionResult Categories()
+        {
+            var userId = User.GetUserId();
+
+            var vm = new TasksViewModel
+            {
+                Categories = _categoryRepository.GetCategories(userId)
+            };
+
+            return View(vm);
+        }
+
+        public IActionResult Category(int id = 0)
+        {
+            var userId = User.GetUserId();
+            var category = id == 0 ? new Category { Id = 0, UserId = userId } : _categoryRepository.Get(id, userId);
+
+            var vm = new TaskViewModel
+            {
+                Categories = _categoryRepository.GetCategories(userId),
+                Heading = id == 0 ? "Dodawanie nowej kategorii" : "Edytowanie kategorii"
+            };
+
+            return View(vm);
+        }
+
         
     }
 }
---
+++ Persistence/Repositories/CategoryRepository.cs
@@ -0,0 +1,25 @@
+﻿using MyTasks.Core.Models.Domains;
+using MyTasks.Persistence;
+
+namespace MyTasks.Persistence.Repositories
+{
+    public class CategoryRepository
+    {
+        private ApplicationDbContext _context;
+
+        public CategoryRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Category> GetCategories(string userId)
+        {
+            return _context.Categories.Where(x => x.UserId == userId).ToList();
+        }
+
+        public Category Get(int id, string userId)
+        {
+            return _context.Categories.Single(x => x.UserId == userId && x.Id == id);
+        }
+    }
+}
