579d292112e75a8e928c6d9b272301126efe9736
Jakub Zięba <jakubzieba7@gmail.com>
2023-09-30 11:29:37 +0200

    Added Categories view and Category view model

---
+++ Core/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,10 @@
+﻿using MyTasks.Core.Models.Domains;
+
+namespace MyTasks.Core.ViewModels
+{
+    public class CategoryViewModel
+    {
+        public string Heading { get; set; }
+        public Category Category { get; set; }
+    }
+}
